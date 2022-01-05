80f0d4fdc2cbeb902a9d9131b6a59308e73d495f
JonCabot <jfc0053@gmail.com>
2022-01-05 17:43:22 -0600

    completed assignment

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,9 +1,22 @@
-function fibonacci(num) {
-  // type your code here
+function fibonacci(num){
+  if (num < 2){
+    return num;
+  }
+  let lastTwoIndexes = [0, 1];
+
+  for(let i = 0; i < num - 1; i++){
+    const sum = lastTwoIndexes[0] + lastTwoIndexes[1];
+    lastTwoIndexes = [lastTwoIndexes[1], sum]
+  }
+  return lastTwoIndexes[1];
 }
+
 
 if (require.main === module) {
   // add your own tests in here
+  console.log("Expecting: 55");
+  console.log("=>", fibonacci(10))
+
   console.log("Expecting: 0");
   console.log("=>", fibonacci(0));
 
@@ -21,4 +34,11 @@ if (require.main === module) {
 module.exports = fibonacci;
 
 // Please add your pseudocode to this file
+//if num input is 0 or 1, it's output is 0 or 1.
+//create an array to store values to add together and ultimately return as final output for input num.
+//start with 0 index and 1 index
+//create a loop, and loop to the nth value - 1, because our sum of the previous 2 indexes will yield nth value(output).
+//create a variable to store index sums
+//push the final index 1 and sum into lastTwoIndexes array.  This could keep going if needed
+
 // And a written explanation of your solution
